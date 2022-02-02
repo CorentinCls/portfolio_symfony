70221afa8de84d0b8a9cdd7cb46d0826f6e77d0c
CorentinChalons <chalons.corentin@gmail.com>
2022-02-02 22:18:04 +0100

    Create entity Project and Skill

--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -6,7 +6,6 @@ use App\Repository\SkillRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use phpDocumentor\Reflection\Project;
 
 #[ORM\Entity(repositoryClass: SkillRepository::class)]
 class Skill
@@ -16,7 +15,7 @@ class Skill
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'text')]
+    #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
     #[ORM\Column(type: 'integer')]
